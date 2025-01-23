8cd1e4b05d01be6f8047c1c2615520f61295ffa9
eduardoC-hub <eduardomarquiori99@gmail.com>
2025-01-23 12:24:18 -0400

    corrigir

--- src/routes/DW2/+page.svelte
+++ src/routes/DW2/+page.svelte
@@ -1,18 +1,11 @@
-<script>
-    function voltar() {
-     
-    }
-  </script>
-  
+  <div class="container">
+    <h1>Bem-vindo à outra página!</h1>
+    <a class = "btn" href = "/DW1">Voltar</a>
+  </div>
+
   <style>
     .container {
       text-align: center;
       padding: 20px;
     }
   </style>
-  
-  <div class="container">
-    <h1>Bem-vindo à outra página!</h1>
-    <a class = "btn" href = "/DW1">Voltar</a>
-  </div>
-  
