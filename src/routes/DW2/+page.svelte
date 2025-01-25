011c017cfaecec4a24eb2d8ceaa7994488fda045
Calestini1 <edu.marqui.cale@gmail.com>
2025-01-24 22:47:18 -0300

    a

--- src/routes/DW2/+page.svelte
+++ src/routes/DW2/+page.svelte
@@ -158,8 +158,8 @@
       border-radius: 15px;
     }
     .card-img-top {
-      width: 100%;
-      height: 250px; 
+      width: 50%;
+      height: 150px; 
       object-fit: cover; 
     }
   </style>
