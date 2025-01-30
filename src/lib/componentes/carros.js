3e48b752ae6d554a5a8bdbe64010b80df072939d
eduardoC-hub <eduardomarquiori99@gmail.com>
2025-01-30 11:45:51 -0400

    a

---
+++ src/lib/componentes/carros.js
@@ -0,0 +1,5 @@
+export let cars = [
+    { name: "Ferrari 488", price: "$280,000", image: "ferrari.jfif", description: "Um carro esportivo de alto desempenho com motor V8 e design deslumbrante." },
+    { name: "Lamborghini Aventador", price: "$393,000", image: "lambo.webp", description: "Um supercarro com motor V12 potente e luxo incomparável." },
+    { name: "Rolls-Royce Phantom", price: "$460,000", image: "rolls.jpg", description: "O epítome da elegância e sofisticação em veículos de luxo." },
+  ];
